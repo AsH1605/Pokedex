9cb491964e454c7166fd6493209cefaba1693b22
Akshita Tiwary <akshitatiwary16@gmail.com>
2025-05-19 15:08:24 +0530

    initialize project and add dependencies

---
+++ app/src/main/java/com/cookie/pokedex/ui/theme/Type.kt
@@ -0,0 +1,40 @@
+package com.cookie.pokedex.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.cookie.pokedex.R
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+val RobotoCondensed = FontFamily(
+    Font(R.font.roboto_condensed_light, FontWeight.Light),
+    Font(R.font.roboto_condensed_regular, FontWeight.Normal),
+)
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = RobotoCondensed,
+        fontWeight = FontWeight.Light,
+        fontSize = 11.sp
+    )
+    // Add more styles as needed: headlineSmall, bodyMedium, etc.
+)
